a650372f21060677ebda4323857c6dca38d3bf2f
SteveIsReal <pyn.zabulata@gmail.com>
2025-04-09 13:10:24 +0700

    Database Update

--- shopping/admin.py
+++ shopping/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
-
+from .models import Class, Teacher, Student
 # Register your models here.
+@admin.register(Class)
+class AdminClass(admin.ModelAdmin):
+    list_display = ['class_name','class_day','class_time','teacher']
+
+@admin.register(Teacher)
+class AdminTeacher(admin.ModelAdmin):
+    list_display = ['name','email','phone']
+
+@admin.register(Student)
+class AdminStudent(admin.ModelAdmin):
+    list_display = ['name']
--- shopping/models.py
+++ shopping/models.py
@@ -1,3 +1,43 @@
 from django.db import models
 
-# Create your models here.
+class Teacher(models.Model):
+    name = models.CharField(max_length=100)
+    phone = models.CharField(max_length=10)
+    email = models.EmailField()
+
+    def __str__(self):
+        return self.name
+
+class Student(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+class Class(models.Model):
+    weekday = [
+            ('MONDAY','Monday'),
+            ('TUESDAY','Tuesday'),
+            ('WEDNESDAY','Wednesday'),
+            ('THRUSDAY','Thrusday'),
+            ('FRIDAY','Friday'),
+            ('SATURDAY','Saturday'),
+            ('SUNDAY','Sunday')
+            ]
+    times = [
+            ('10.00-12.00','10.00-12.00'),
+            ('13.00-15.00','13.00-15.00'),
+            ('16.00-18.00','16.00-18.00'),
+            ('17.00-19.00','17.00-19.00')
+            ]
+    class_name = models.CharField(max_length=50)
+    class_day = models.CharField(max_length=20, choices=weekday)
+    class_time = models.CharField(max_length=11, choices=times)
+    teacher = models.ForeignKey(Teacher, on_delete=models.CASCADE)
+    students = models.ManyToManyField(Student)
+
+    def __str__(self):
+        return self.class_name
+
+
+
